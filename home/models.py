a3845cbd72e79fb211fd66d0ca1f6e83689bcc23
vahe grigorian <vahegrigorian447@gmail.com>
2023-02-23 14:25:31 +0330

    time line and appointment model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+
+
+class Timeline(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    time = models.CharField(max_length=200)
+    icon = models.ImageField(upload_to='images/icons')
+
+    def __str__(self):
+        return f'{self.title} - {self.time}'
+
+
+class Appointment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=300)
+    email_address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=30)
+    additional_message = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f'{self.full_name} - {self.phone_number}'
--- madicCare/settings.py
+++ madicCare/settings.py
@@ -131,3 +131,4 @@ MEDIA_ROOT = path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.User'
